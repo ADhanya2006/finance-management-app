26fdf37ab59f3f2d5d80be548d804b3b322d4dad
ADhanya2006 <dhanyasri919@gmail.com>
2025-05-12 15:08:34 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from supabase import create_client, Client
+from supabase_py import create_client, Client
 
 st.set_page_config(page_title="Login", layout="centered")
 st.title("🔐 Finance App Login")
