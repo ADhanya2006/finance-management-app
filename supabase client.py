11cb312a6996590dd63c56c6b6b729fb0095091a
ADhanya2006 <dhanyasri919@gmail.com>
2025-05-12 15:10:14 +0530

    Update supabase client.py

--- supabase client.py
+++ supabase client.py
@@ -1,6 +1,6 @@
 from supabase import create_client, Client
-import os
 
-SUPABASE_URL = os.getenv("https://supabase.com/dashboard/project/pfgbsstvlwcseckewnti")
-SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmZ2Jzc3R2bHdjc2Vja2V3bnRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMzk1MjcsImV4cCI6MjA2MjYxNTUyN30.s6939uIOyWyp6Fs1jYwgBiBHBOqd6-Tpgirzvn-InXk")
+SUPABASE_URL = "https://pfgbsstvlwcseckewnti.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmZ2Jzc3R2bHdjc2Vja2V3bnRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMzk1MjcsImV4cCI6MjA2MjYxNTUyN30.s6939uIOyWyp6Fs1jYwgBiBHBOqd6-Tpgirzvn-InXk"
+
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
